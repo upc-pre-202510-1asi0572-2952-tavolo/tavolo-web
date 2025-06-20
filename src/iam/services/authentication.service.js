aa60cc231545e0ff08bdd633f606a670a370bad5
FranzJairLTV <u202012378@upc.edu.pe>
2025-06-20 15:17:45 -0500

    fix: authentication

--- src/iam/services/authentication.service.js
+++ src/iam/services/authentication.service.js
@@ -2,12 +2,14 @@ import http from "../../shared/services/http-common.js";
 
 /**
  * Service class for authentication.
+ * @summary
+ * This class contains methods for signing in and signing up.
  */
 export class AuthenticationService {
     /**
      * Sign in method.
-     * @param signInRequest
-     * @returns {Promise}
+     * @param signInRequest {@link SignInRequest} - The sign in request.
+     * @returns {Promise<http.AxiosResponse<SignInResponse>>}
      */
     signIn(signInRequest) {
         return http.post("/auth/sign-in", signInRequest);
@@ -16,9 +18,9 @@ export class AuthenticationService {
     /**
      * Sign up method.
      * @param signUpRequest
-     * @returns {Promise}
+     * @returns {Promise<http.AxiosResponse<SignUpResponse>>}
      */
     signUp(signUpRequest) {
-        return http.post("/auth/sign-up", signUpRequest);
+        return http.post("/authentication/sign-up", signUpRequest);
     }
 }
